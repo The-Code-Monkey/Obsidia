486eecaaa712f5abf08292a5956e3022c172917f
Andy Wilson <andy.wilson1234@gmail.com>
2026-06-10 22:29:08 +0100

    Initialize COM1 serial port and log messages
    Added serial port initialization and logging messages.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const limine = @import("limine");
+const serial = @import("serial.zig");
 
 // Request a framebuffer from the bootloader. 
 // Limine will read this struct, set up the screen, and populate the response with the memory address.
@@ -7,9 +8,15 @@ export var framebuffer_request: limine.FramebufferRequest = .{};
 
 // The entry point called by Limine. It must use the C calling convention.
 export fn _start() callconv(.C) noreturn {
-    
-    // Here is where you will eventually initialize memory, GDT, IDT, and drivers.
-    // For now, we secure the system state by halting the CPU.
+
+    // Initialize the COM1 serial port
+    serial.init();
+
+    // Send our first formatted log message out of the VM!
+    serial.print("========================================\n", .{});
+    serial.print("[OBSIDIA] Kernel initialized successfully.\n", .{});
+    serial.print("[OBSIDIA] Running on modern x86_64 architecture.\n", .{});
+    serial.print("========================================\n", .{});
 
     while (true) {
         // x86_64 specific instruction to halt the processor until the next interrupt
